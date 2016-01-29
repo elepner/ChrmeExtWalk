aeb25184056a938fe9fc46fe5810af2c2861539a
Eduard Y. Lepner <elepner@dataeast.com>
2016-01-29 19:48:08 +0600

    Added parsing walkme urls.

--- background.js
+++ background.js
@@ -2,10 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var WALK_ME_URL = "cdn.walkme.com"
+
 function htmlReturned(domContent) {
     var el = document.createElement('html');
     el.innerHTML = domContent;
-    var script = el.getElementsByTagName('script');
+    var scripts = el.getElementsByTagName('script');
+    var walkmeObject;
+    for(var i = 0; i < scripts.length; i++){
+        var scriptElement = scripts[i];
+        var parser = document.createElement('a');
+        parser.href = scriptElement.src;
+        if(parser.hostname && parser.hostname.indexOf(WALK_ME_URL) > -1){
+            walkmeObject = {};
+            var urlParams = parser.pathname.split('/');
+            walkmeObject.userId = urlParams[2];
+            walkmeObject.production = !(parser.pathname.indexOf('test') > -1);
+            walkmeObject.protocol = parser.protocol;
+            walkmeObject.host = parser.hostname;
+            walkmeObject.async = !!scriptElement.getAttribute('async');
+            break;
+        }
+    }
+    return walkmeObject;
+    
     
 }
 
@@ -14,7 +34,14 @@ function htmlReturned(domContent) {
 chrome.browserAction.onClicked.addListener(function (tab) {
 
 
-    chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, htmlReturned);
+    chrome.tabs.sendMessage(tab.id, { text: 'report_back' }, function(dom){
+        var walkmeObject = htmlReturned(dom);
+        var paramString = "";
+        Object.keys(walkmeObject).forEach(function (key) {
+            paramString += key + "=" + walkmeObject[key] + "&"
+        });
+        chrome.windows.create({ url: "info.html?" + tab.id, type: "popup", width: 800, height: 600 });
+    });
 
     //chrome.windows.create({ url: "headers.html?", type: "popup", width: 800, height: 600 });
 });
